be3872ab684660aaf3b367c01aaf402a1773d58d
stevewu <42365700+Stevewu1234@users.noreply.github.com>
2021-04-30 16:38:29 +0800

    Add files via upload

---
+++ contracts/NFTCreation_v0/Collection_factory.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.6.0;
+
+import "../../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "./Interface/ICollection_Factory.sol";
+import "./Collection.sol";
+
+contract Collection_Factory is Ownable {
+    mapping(address => address) internal collectionowner;
+    address[] public collections;
+
+    /** =================== mutative function =================== */
+    function create(
+        string memory _name,
+        string memory _symbol,
+        string memory _baseuri,
+        address owner
+    ) external returns (address) {
+        require( owner != address(0), "you must set a address");
+        Collection collectionaddress = new Collection(_name,_symbol,_baseuri,owner);
+        collectionowner[address(collectionaddress)] = owner;
+        collections.push(address(collectionaddress));
+        emit createNFTContract( _name, _symbol, address(collectionaddress));
+        return address(collectionaddress);
+    }
+
+
+    /** =================== view =================== */
+    function getlength() external view returns (uint){
+        return collections.length;
+    }
+
+    function getcollection(address nftaddress) external view returns (address) {
+        return collectionowner[nftaddress];
+    }
+
+
+    /** =================== event =================== */
+    event createNFTContract(string indexed name, string indexed symbol,address indexed collectionaddress);
+
+}
